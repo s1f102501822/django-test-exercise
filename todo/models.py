fe82a55b7b2d3f4e501fb38f945bfc0077956dd1
1f10250182 <s1f102501822@iniad.org>
2026-06-18 11:23:25 +0900

    Taskモデルにメゾットを追加

--- todo/models.py
+++ todo/models.py
@@ -7,3 +7,16 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
+
+def is_overdue(self, dt):
+        if self.due_at is None:
+            return False
+        return self.due_at < dt
+
+def test_is_overdue_future(self):
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
+        task = Task(title='task1', due_at=due)
+        task.save()
+
+        self.assertFalse(task.is_overdue(current))
